212798d0ae904e53041617ee34962f7cf7067ba4
AriyanMLZM <112932248+AriyanMLZM@users.noreply.github.com>
2022-09-06 21:28:53 +0430

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,69 @@
+document.getElementById('btn1').addEventListener("click", function(){
+  document.getElementsByClassName("txt")[0].style.color = "#7a7a7a";
+  document.getElementsByClassName("txt")[1].style.color = "#7a7a7a";
+  document.getElementById('sec').style.borderColor = "#7a7a7a";
+  document.body.style.background = "#222";
+})
+
+document.getElementById('btn2').addEventListener("click", function(){
+  document.getElementsByClassName("txt")[0].style.color = "#0e7fb0";
+  document.getElementsByClassName("txt")[1].style.color = "#0e7fb0";
+  document.getElementById('sec').style.borderColor = "#0e7fb0";
+  document.body.style.background = "#222";
+})
+
+document.getElementById('btn3').addEventListener("click", function(){
+  document.getElementsByClassName("txt")[0].style.color = "#03b05c";
+  document.getElementsByClassName("txt")[1].style.color = "#03b05c";
+  document.getElementById('sec').style.borderColor = "#03b05c";
+  document.body.style.background = "#222";
+})
+
+document.getElementById('btn4').addEventListener("click", function(){
+  document.getElementsByClassName("txt")[0].style.color = "#9e1818";
+  document.getElementsByClassName("txt")[1].style.color = "#9e1818";
+  document.getElementById('sec').style.borderColor = "#9e1818";
+  document.body.style.background = "#222";
+})
+
+document.getElementById('btn5').addEventListener("click", function(){
+  document.getElementsByClassName("txt")[0].style.color = "#e3c21e";
+  document.getElementsByClassName("txt")[1].style.color = "#e3c21e";
+  document.getElementById('sec').style.borderColor = "#e3c21e";
+  document.body.style.background = "#222";
+})
+
+document.getElementById('btn6').addEventListener("click", function(){
+  document.getElementsByClassName("txt")[0].style.color = "#7a7a7a";
+  document.getElementsByClassName("txt")[1].style.color = "#7a7a7a";
+  document.getElementById('sec').style.borderColor = "#7a7a7a";
+  document.body.style.background = "#fffced";
+})
+
+document.getElementById('btn7').addEventListener("click", function(){
+  document.getElementsByClassName("txt")[0].style.color = "#0e7fb0";
+  document.getElementsByClassName("txt")[1].style.color = "#0e7fb0";
+  document.getElementById('sec').style.borderColor = "#0e7fb0";
+  document.body.style.background = "#fffced";
+})
+
+document.getElementById('btn8').addEventListener("click", function(){
+  document.getElementsByClassName("txt")[0].style.color = "#03b05c";
+  document.getElementsByClassName("txt")[1].style.color = "#03b05c";
+  document.getElementById('sec').style.borderColor = "#03b05c";
+  document.body.style.background = "#fffced";
+})
+
+document.getElementById('btn9').addEventListener("click", function(){
+  document.getElementsByClassName("txt")[0].style.color = "#9e1818";
+  document.getElementsByClassName("txt")[1].style.color = "#9e1818";
+  document.getElementById('sec').style.borderColor = "#9e1818";
+  document.body.style.background = "#fffced";
+})
+
+document.getElementById('btn10').addEventListener("click", function(){
+  document.getElementsByClassName("txt")[0].style.color = "#e3c21e";
+  document.getElementsByClassName("txt")[1].style.color = "#e3c21e";
+  document.getElementById('sec').style.borderColor = "#e3c21e";
+  document.body.style.background = "#fffced";
+})
